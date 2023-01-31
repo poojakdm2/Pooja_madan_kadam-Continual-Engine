810e49368ea7197ed84eb5a22dd85fa522fc68aa
Kadam Pooja Madan <79741120+poojakdm2@users.noreply.github.com>
2023-01-31 12:57:14 +0530

    Add files via upload

---
+++ app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+
+
+})
+export class AppComponent {
+
+  navbarfixed:boolean = false;
+
+  @HostListener('window:scroll',['$event']) onscroll(){
+    if(window.scrollY > 100)
+    {
+      this.navbarfixed = true;
+    }
+    else
+    {
+      this.navbarfixed = false;
+    }
+  }
+
+}
